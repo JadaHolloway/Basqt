755d7a6d2c65267668edf6e2d0f97302646b24b5
JadaCakes <jadacakes@JadaCakess-MacBook-Pro.local>
2026-03-27 15:58:29 -0400

    Initial Commit

---
+++ Basqt/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Basqt
+//
+//  Created by JadaCakes on 3/27/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
