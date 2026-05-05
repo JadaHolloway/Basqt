2c389cf6aea2c8ce135634f6fb55bf76b3ecdd4d
micki222023F <micki22@vt.edu>
2026-05-05 13:30:37 -0400

    image slider and timer

--- Basqt/Tab View Tabs/Image Slider/FoodsSlide.swift
+++ Basqt/Tab View Tabs/Image Slider/FoodsSlide.swift
@@ -7,13 +7,19 @@
 //
 
 import SwiftUI
+import SwiftData
+internal import Combine
 
 struct FoodSlide: View {
-    
+    @Query private var recipes: [Recipe]
     // Default selected background color
     @State private var selectedBgColor = Color.gray.opacity(0.1)
     
     @State private var showAlertMessage = false
+    @State private var selectedTab = 0
+    @State private var timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+    
+  
     //add timer to this?
     var body: some View {
         NavigationStack {
@@ -29,52 +35,64 @@ struct FoodSlide: View {
                             .padding()
                     }
                     Spacer()
-                    
-                    TabView {
-                        // beachStructList is a global array of Beach structs given in TravelGuideData.swift
-                        ForEach(beachStructList) { beach in
-                            VStack {
-                                Link(destination: URL(string: beach.websiteUrl)!) {
-                                    Text(beach.title)
-                                        .font(.headline)
-                                        .multilineTextAlignment(.center)
-                                        .padding()
+                    if recipes.isEmpty {
+                        Text("No recipes are available.")
+                        Spacer()
+                    } else {
+                        TabView(selection: $selectedTab) {
+                            
+                            ForEach(0..<recipes.count, id: \.self) { index in
+                                let recipe = recipes[index]
+                                VStack {
+                                    NavigationLink(destination: RecipeDetails(recipe: recipe)) {
+                                        Text(recipe.name)
+                                            .font(.headline)
+                                            .multilineTextAlignment(.center)
+                                            .padding()
+                                    }
+                                    Image(recipe.photoFilename)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
                                 }
-                                Image(beach.photoFilename)
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
+                                .tag(index)
                             }
-                        }
-                    }   // End of TabView
-                    .tabViewStyle(PageTabViewStyle())
-                    .onAppear() {
-                        UIPageControl.appearance().currentPageIndicatorTintColor = .black
-                        UIPageControl.appearance().pageIndicatorTintColor = .gray
-                    }
-                    .navigationTitle("Your Best Foods in the World")
-                    .toolbarTitleDisplayMode(.inline)
-                    .toolbar {
-                        // Place the Information button on right side of the toolbar
-                        ToolbarItem(placement: .topBarTrailing) {
-                            Button(action: {
-                                showAlertMessage = true
-                                alertTitle = "Source Reference"
-                                alertMessage = "24 Best Island Beaches in the World data and photos are taken from: https://www.cntraveler.com"
-                            }) {
-                                Image(systemName: "info.circle")
-                                    .imageScale(.small)
-                                    .font(Font.title.weight(.light))
+                        }   // End of TabView
+                        .tabViewStyle(PageTabViewStyle())
+                        
+                        .onReceive(timer) { _ in
+                            withAnimation {
+                                selectedTab = (selectedTab + 1) % recipes.count
                             }
                         }
                     }
-                    .alert(alertTitle, isPresented: $showAlertMessage, actions: {
-                          Button("OK") {}
-                        }, message: {
-                          Text(alertMessage)
-                        })
                     
-                }   // End of VStack
-            }   // End of ZStack
-        }   // End of NavigationStack
-    }   // End of body var
+                }
+            }
+            .onAppear() {
+                UIPageControl.appearance().currentPageIndicatorTintColor = .black
+                UIPageControl.appearance().pageIndicatorTintColor = .gray
+            }
+            .navigationTitle("Your Best Foods in the World")
+            .toolbarTitleDisplayMode(.inline)
+            .toolbar {
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+                        showAlertMessage = true
+                        alertTitle = "Recipe Slider"
+                        alertMessage = "Swipe manually or let the timer show you the best recipes in your Basqt"
+                    }) {
+                        Image(systemName: "info.circle")
+                            .imageScale(.small)
+                            .font(Font.title.weight(.light))
+                    }
+                }
+            }
+            .alert(alertTitle, isPresented: $showAlertMessage) {
+                Button("OK") {}
+            } message: {
+                Text(alertMessage)
+            }
+        }
+    }
 }
